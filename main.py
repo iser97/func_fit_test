bd3ce80303cc94eef5fb846ab3f2ee2ebef1fa75
iser97 <1156653418@qq.com>
2021-12-06 21:31:17 +0800

    master

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as sst
 from scipy.optimize import curve_fit
+
+#用于计算Lxx, Lyy
+def laa(x):
+    x_mean = np.mean(x)
+    lxx = np.sum((x-x_mean)**2)
+    return lxx
+
+#用于计算Lxy
+def lab(x,y):
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    lxy = np.sum((x-x_mean)*(y-y_mean))
+    return lxy
 
 def read_data(data_root):
     ''' read 601666.csv file and preprocess this file
@@ -29,10 +43,8 @@ def read_data(data_root):
 def func(x, alpha, beta):
     return alpha + beta*x
 
-if __name__ == "__main__":
-    X, Y, X_t, Y_t = read_data("./601666.csv")
+def question_one(X_t, Y_t):
     popt, pcov = curve_fit(func, X_t, Y_t)
-    
     alpha = popt[0]
     beta = popt[1]
     print("alpha = ", alpha)
@@ -45,6 +57,47 @@ if __name__ == "__main__":
     plt.legend(loc=4) #指定legend的位置右下角
     plt.title('curve_fit')
     plt.savefig("fit_result.jpg")
-    plt.show()
+    # plt.show()
+    return alpha, beta
+
+def question_two(x, y, alpha=0.05):
+    '''
+    Args:
+        alpha: 是置信区间的取值，0.05则是95%的置信区间
+    '''
+    n = len(x)
+    lxx = laa(x)
+    lyy = laa(y)
+    lxy = lab(x, y)
+
+    R = lxy/(np.sqrt(lxx) * np.sqrt(lyy))
+    R2 = R*R   #计算相关系数与决定系数
+
+    b_est = lxy/lxx  #计算b估计
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    a_est = y_mean - b_est * x_mean   #计算a估计
+    Qe = lyy - b_est * lxy
+    sigma_est2 = Qe / (n - 2)
+
+    sigma_est = np.sqrt(sigma_est2) #sigma估计
+
+    test = np.abs(b_est * np.sqrt(lxx))/sigma_est
+    test_level = sst.t.ppf(1 - alpha/2, df=n - 2)
+    linear_test = test > test_level   #线性回归检验
+
+    #a,b的置信区间
+    b_int = [b_est - test_level * sigma_est / np.sqrt(lxx), b_est + test_level * sigma_est / np.sqrt(lxx)]
+    a_int = [y_mean - b_int[1] * x_mean, y_mean - b_int[0] * x_mean]
+
+    return a_est, b_est, a_int, b_int
+
+
+if __name__ == "__main__":
+    X, Y, X_t, Y_t = read_data("./601666.csv")
+    alpha, beta = question_one(X_t, Y_t)
+    print("回归拟合得到的alpha=", alpha, "  beta=", beta)
+    alpha, beta, alpha_int, beta_int = question_two(X_t, Y_t, alpha=0.05)
+    print("alpha的统计量=", alpha, "   beta的统计量=", beta, "alpha的置信区间：", alpha_int,  "    beta的置信区间：", beta_int)
     
  
